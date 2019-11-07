dfce3d48228d4949a0150ad6fcdd509df6bb56de
Rayhan <mrayferdinand@student.telkomuniversity.ac.idgit config --global user.name Rayhangit config --global user.email mrayferdinand@student.telkomuniversity.ac.id>
2019-11-07 23:06:26 +0700

    Update lagi.

---
+++ tubesIMPAL/application/views/user/page_buatakun.php
@@ -0,0 +1,48 @@
+<style type="text/css">
+		.container {
+			text-align: center;
+		}
+		.container .ketik {
+			width: 40%;
+			height: 50px;
+			margin: 10px;
+			border-radius: 30px;
+		}
+		.container .tgl {
+			border-radius: 30px;
+			width: 12.8%;
+			height: 40px;
+		}
+		.b1 {
+			background-color: #f44336;
+			border-radius: 30px;
+			width: 10%;
+			height: 40px;
+		}
+		.b2 {
+			background-color: #008CBA;
+			border-radius: 30px;
+			width: 10%;
+			height: 40px;
+		}
+</style>
+
+<div class="text" style="text-align: center;">
+		<h1>Let's Join e-Book!</h1>
+		<h3 style="color: grey">start your journey now</h3>
+	</div>
+	<div class="container" style="background: url(<?=base_url('assets/img/perpus.jpg')?> )">
+		<div class="input">
+			<br><br>
+			<input type="text" placeholder="username" name="username" class="ketik"><br>
+			<input type="text" placeholder="email" name="email" class="ketik"><br>
+			<input type="text" placeholder="password" name="password" class="ketik"><br>
+			<input type="text" placeholder="bulan" name="bulan" class="tgl">
+			<input type="text" placeholder="tanggal" name="tanggal" class="tgl">
+			<input type="text" placeholder="tahun" name="tahun" class="tgl">
+			<br><br>
+			<button class="b1">Google </button>
+			<button class="b2">Mulai menulis</button>
+			<br><br><br>
+		</div>
+	</div>
