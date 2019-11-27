c29e40a75b01dec5ae931dc900914e50f61aeb08
ekkyyps <ekkyprastika@gmail.com>
2019-11-27 18:52:11 +0700

    ui beranda reader

--- tubesIMPAL/application/views/user/page_homepage.php
+++ tubesIMPAL/application/views/user/page_homepage.php
@@ -1,8 +1,8 @@
 
 <style type="text/css">
-	.row{
-		height: 25vw;
-	}
+  /*.row{
+    height: 25vw;
+  }
   .row.heading h2 {
     color: #000000;
     font-size: 52.52px;
@@ -17,6 +17,38 @@
     margin:0;
     padding:0;
     list-style:none;
+  }*/
+  h3{
+    text-align: center; 
+    padding-top: 7px;
+  }
+  .card .col-xl-2{
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 15.5vw;
+    height: 23vw;
+    border-radius:5px;
+    background-color: white; 
+    margin-right: 15px;
+    font-family: "Verdana", sans-serif;
+    font-weight: bold;
+    margin-top: 55px;
+  }
+  .container .card .col-xl-2 .btn{
+    background-color:#14868c;
+    border-radius: 5px;
+    width: 15.5vw;
+    height: 40px;
+    color: white;
+    border-color: none;
+    float: right;
+    font-family: Courier New;
+    font-weight: bold;
+    margin-top: 20vw;
+    margin-right: 1px;
+  }
+  .card .col-xl-2 .btn:hover{
+    opacity: 0.8; 
   }
 
 </style>
@@ -27,7 +59,74 @@
 <?php $data['cerpen']= ('cerpen'); ?>
 <?php $data['biografi']= ('biografi'); ?>
 <?php $data['xxx']= ('xxx'); ?>
+
 <div class="container">
+  <div class="card">
+      <div class="row-inline">
+          <div class="col-xl-2 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+              <h3>NOVEL</h3>
+            </div>
+            <input type="submit" class="btn" value="Telusuri">
+          </div>
+
+          <div class="col-xl-2 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+              <h3>JURNAL</h3>
+            </div>
+            <input type="submit" class="btn" value="Telusuri">
+          </div>
+
+          <div class="col-xl-2 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+              <h3>PAPER</h3>
+            </div>
+            <input type="submit" class="btn" value="Telusuri">
+          </div>
+
+          <div class="col-xl-2 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+              <h3>CERPEN</h3>
+            </div>
+            <input type="submit" class="btn" value="Telusuri">
+          </div>
+
+          <div class="col-xl-2 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+              <h3>BIOGRAFI</h3>
+            </div>
+            <input type="submit" class="btn" value="Telusuri">
+          </div>
+    
+      </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="container">
   <section class="padding-lg">
     <div class="container">
       <div class="row heading heading-icon">
@@ -81,4 +180,4 @@
     	</div>
 	</a>
   </div>
-</div>
+</div> -->
